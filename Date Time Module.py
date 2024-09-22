4ed101ccbaf5526ed7030fb5cc96bf60d62dfa40
AmlanAnupam-2007 <amlananupam2007@gmail.com>
2024-09-22 19:17:12 +0530

    L-3

---
+++ Date Time Module.py
@@ -0,0 +1,5 @@
+import datetime 
+a=datetime.datetime.now()
+print(a)
+import calendar
+print(calendar.calendar(3024))
---
+++ check for odd or even.py
@@ -0,0 +1,10 @@
+a=int(input("enter the number"))
+if a>=50:
+    print("Yes the number is greater than 50")
+    if a%2==0:
+        print("Even")
+    else:
+        print("Odd")
+else:
+    print("Error")
+
